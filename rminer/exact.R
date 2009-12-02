260c64d099c07d466858cb785c7f23f98aea0c74
DecoDe <code723@gmail.com>
2009-12-02 17:06:05 +0800

    update

--- rminer/exact.R
+++ rminer/exact.R
@@ -1,6 +1,26 @@
 # An util get information form id, like birthday, gender, area
 rminer.exact_form_id <- function(id) {
-
+  if(nchar(id) != 18 && nchar(id) != 15)
+    return
+  district <- substr(id, 0, 6)
+  # ID digital is 18 like: xxxxxx    xxxxxxxx   xxx    x
+  #                       (district)(birthday)(gender)(crc code)
+  if(nchar(id) == 18) {
+    age <- as.integer(format(Sys.Date(), "%Y")) - as.integer(substr(id, 7, 10))
+    #birthday <- as.Date(substr(id, 7, 14), "%Y%m%d")
+    birthday <- substr(id, 11, 14)
+    gender <- as.integer(substr(id, 15, 17)) %% 2
+  }
+  # ID digital is 15 like: xxxxxx    xxxxxx    xxx
+  #                       (district)(birthday)(gender)
+  else {
+    age <- as.integer(format(Sys.Date(), "%y")) - as.integer(substr(id, 7, 8))
+    #birthday <- as.Date(substr(id, 7, 12), "%Y%m%d")
+    birthday <- substr(id, 9, 12)
+    gender <- as.integer(substr(id, 13, 15)) %% 2
+  }
+  lst <- list("district" = district, "age" = age, "birthday" = birthday, "gender" = gender)
+  return(lst)
 }
 
 rminer.load_csv  <- function(filename) {
@@ -10,10 +30,18 @@ rminer.load_csv  <- function(filename) {
 
 rminer.exact <- function(data, column_name) {
   for(i in 1:nrow(data)) {
-    field = data[i, column_name]
-    rminer.exact_form_id(field)
+    if(is.na(data[i, column_name]))
+      next
+    field = as.character(data[i, column_name])
+    info <- rminer.exact_form_id(field)
+    # Add extracted information to origin dataset
+    for(j in 1:length(names(info))) {
+      data[i, names(info)[j]] <- info[names(info)[j]]
+    }
   }
+  return(data)
 }
 
 data <- rminer.load_csv('~/Downloads/XJG.csv')
-a <- as.character(data[1,4])
+result <- rminer.exact(data, 4)
+write.csv(result, file="new.csv")
