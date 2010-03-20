cd27836b7a2cff4cd86eeee67424673aad7a4300
DecoDe <code723@gmail.com>
2010-03-20 10:25:40 +0800

    update fuzzy method

--- fuzzy/fuzzy.R
+++ fuzzy/fuzzy.R
@@ -20,10 +20,9 @@ fuzzy.judge_matrix <- function(source) {
       temp[j, judge[i, j]+1] <- temp[j, as.numeric(judge[i, j])+1] + 1
     }
   }
-  print(nwidth)
   temp <- temp / nlength
   print(temp)
-  return(judge)
+  return(temp)
 }
 
 fuzzy.synthesize <- function(weight_data, judge_data) {
@@ -32,15 +31,7 @@ fuzzy.synthesize <- function(weight_data, judge_data) {
 }
 
 weight_data <- read.csv("result.csv", sep=",", header=FALSE)
-#nlength = nrow(weight_data)
-#nwidth = ncol(weight_data)
 pre <- fuzzy.load_source("~/Downloads/Taobao.csv")
-print(pre)
-
 judge_data <- fuzzy.judge_matrix(pre[, 7:39])
-
-#nlength = nrow(judge_data)
-#nwidth = ncol(judge_data)
-
-judge <- fuzzy.synthesize(t(weight_data[, 3]), pre)
+judge <- fuzzy.synthesize(t(weight_data[, 3]), judge_data)
 print(judge)
