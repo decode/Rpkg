226bcbaa00266a7fddec0cd4faa6086d105dc113
DecoDe <code723@gmail.com>
2010-03-23 14:24:37 +0800

    update

--- fuzzy/fuzzy.R
+++ fuzzy/fuzzy.R
@@ -20,16 +20,9 @@ fuzzy.judge_matrix <- function(source) {
       temp[j, judge[i, j]+1] <- temp[j, as.numeric(judge[i, j])+1] + 1
     }
   }
-<<<<<<< HEAD
-  print(nwidth)
-  temp <- temp / nlength
-  print(temp)
-  return(judge)
-=======
   temp <- temp / nlength
   print(temp)
   return(temp)
->>>>>>> cd27836b7a2cff4cd86eeee67424673aad7a4300
 }
 
 fuzzy.synthesize <- function(weight_data, judge_data) {
@@ -38,21 +31,11 @@ fuzzy.synthesize <- function(weight_data, judge_data) {
 }
 
 weight_data <- read.csv("result.csv", sep=",", header=FALSE)
-<<<<<<< HEAD
-#nlength = nrow(weight_data)
-#nwidth = ncol(weight_data)
-pre <- fuzzy.load_source("~/Downloads/Taobao.csv")
-print(pre)
-
-judge_data <- fuzzy.judge_matrix(pre[, 7:39])
-
-#nlength = nrow(judge_data)
-#nwidth = ncol(judge_data)
-
-judge <- fuzzy.synthesize(t(weight_data[, 3]), pre)
-=======
 pre <- fuzzy.load_source("~/Downloads/Taobao.csv")
 judge_data <- fuzzy.judge_matrix(pre[, 7:39])
 judge <- fuzzy.synthesize(t(weight_data[, 3]), judge_data)
->>>>>>> cd27836b7a2cff4cd86eeee67424673aad7a4300
 print(judge)
+
+print(pre[, 2:3])
+weight <- array(c(1, 0.775, 0.75, 0.75, 0.05, 0.75, 1, 1, 0.7, 0.525), dim=c(1,10))
+
