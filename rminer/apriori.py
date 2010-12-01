ef65ecc55ce890a42cbe10b8041d3cde574791f4
DecoDe <code723@gmail.com>
2010-12-01 18:27:20 +0800

    update

--- rminer/apriori.py
+++ rminer/apriori.py
@@ -3,7 +3,7 @@
 
 from models import *
 from pymmseg import mmseg
-#import rpy2.robjects as robjects
+import orange
 
 class Apriori:
   def fetch(self):
@@ -17,15 +17,31 @@ class Apriori:
       text = i.name
       text = text.encode("utf-8")
       algor = mmseg.Algorithm(text)
+
       str = "|"
+      file = open('data.basket', 'a')  
       for tok in algor:
         str += tok.text.decode('utf-8')
         str += "|"
-      print(str)
       seg.content = str
       session.commit()
+      file.write(self.format(str).encode('utf-8') + "\n")
+
+    file.close()
+
+  def format(self, str):
+    str = str[1:-1].replace("|", ", ")
+    return str
+
+  def analysis(self):
+    data = orange.ExampleTable("data")
+    rules = orange.AssociationRulesSparseInducer(data, support = 0.5)
+    for r in rules:
+      print "%5.3f   %5.3f   %s" % (r.support, r.confidence, r)
+
 
 if __name__ == '__main__':
   apriori = Apriori()
   apriori.fetch()
+  apriori.analysis()
 
