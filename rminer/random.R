d9ed3d0d749f393b399302617e1edf28eabe5d27
DecoDe <code723@gmail.com>
2011-11-13 20:34:22 +0800

    update

--- rminer/random.R
+++ rminer/random.R
@@ -65,10 +65,59 @@ random.init <- function(plist, number=100, timelength=1000) {
 }
 
 credit <- random.init(random.p, c_type, time_length)
+credit_type <- random.credit_type
+
 print(random.credit_type)
-result <- som(data = credit, grid = somgrid(5, 5, "hexagonal"))
-write.csv(cbind(result$unit.classif, random.credit_type, credit), 'random.csv')
+#result <- som(data = credit, grid = somgrid(5, 5, "hexagonal"))
+result <- xyf(scale(credit), classvec2classmat(credit_type),
+              grid = somgrid(5, 5, "hexagonal"), rlen=100)
+write.csv(cbind(result$unit.classif, credit_type, credit), 'random.csv')
+
+plot(result, type="mapping",
+     labels = credit_type, col = credit_type+1,
+     main = "mapping plot")
+plot(result, type="mapping", col = credit_type+1,
+     pchs = credit_type, bgcol = bgcols[as.integer(xyfpredictions)],
+     main = "another mapping plot")
+plot(result, type="codes", main = c("Codes X", "Codes Y"))
 
 # 构造一条新数据
 n <- random.init(random.p, number=1, timelength=time_length)
 #p <- predict(result, n)
+
+
+data(wines)
+set.seed(7)
+kohmap <- xyf(scale(wines), classvec2classmat(wine.classes),
+              grid = somgrid(5, 5, "hexagonal"), rlen=100)
+plot(kohmap, type="changes")
+plot(kohmap, type="codes", main = c("Codes X", "Codes Y"))
+plot(kohmap, type="counts")
+## palette suggested by Leo Lopes
+coolBlueHotRed <- function(n, alpha = 1) {
+  rainbow(n, end=4/6, alpha=alpha)[n:1]
+}
+plot(kohmap, type="quality", palette.name = coolBlueHotRed)
+plot(kohmap, type="mapping",
+     labels = wine.classes, col = wine.classes+1,
+     main = "mapping plot")
+## add background colors to units according to their predicted class labels
+xyfpredictions <- classmat2classvec(predict(kohmap)$unit.predictions)
+bgcols <- c("gray", "pink", "lightgreen")
+plot(kohmap, type="mapping", col = wine.classes+1,
+     pchs = wine.classes, bgcol = bgcols[as.integer(xyfpredictions)],
+     main = "another mapping plot")
+## Another way to show clustering information
+set.seed(7)
+sommap <- som(scale(wines),grid = somgrid(6, 4, "hexagonal"))
+plot(sommap, type="dist.neighbours", main = "SOM neighbour distances")
+## use hierarchical clustering to cluster the codebook vectors
+som.hc <- cutree(hclust(dist(sommap$codes)), 5)
+add.cluster.boundaries(sommap, som.hc)
+## and the same for rectangular maps
+set.seed(7)
+sommap <- som(scale(wines),grid = somgrid(6, 4, "rectangular"))
+plot(sommap, type="dist.neighbours", main = "SOM neighbour distances")
+## use hierarchical clustering to cluster the codebook vectors
+som.hc <- cutree(hclust(dist(sommap$codes)), 5)
+add.cluster.boundaries(sommap, som.hc)
